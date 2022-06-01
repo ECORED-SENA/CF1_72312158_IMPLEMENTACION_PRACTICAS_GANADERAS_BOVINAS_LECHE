7a7fd220647b83ddb8b55c078af207d612160359
SANDRATRIANA <sandrajem23@hotmail.com>
2022-05-31 19:39:30 -0500

    Avance 2

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -51,7 +51,7 @@
     Separador
     #t_2_1
     .titulo-segundo.color-acento-contenido(data-aos="fade-right")
-      h2 1.1 Normatividad
+      h2 2.1 Normatividad
 
     .row.align-items-center.mb-5
       .col-lg-7
@@ -61,13 +61,62 @@
           img.ms-5(src='@/assets/curso/tema2/img05.svg', style="width: 530px").m-auto
 
     h3.text-center Leyes y decretos que rige el CONPES 
+    .tarjeta.tarjeta--amarilla2.p-4.mb-5
+      SlyderA(tipo="b")
+        .row.align-items-center.justify-content-center
+          .col-lg-4
+            h4 CONPES 3375 de 2005.
+            p Política Nacional de Sanidad Agropecuaria e Inocuidad de Alimentos para el Sistema de Medidas Sanitarias y Fitosanitarias- MSF. Que es la introducción del enfoque sistémico con el objeto de articular las entidades con competencias y funciones en materia sanitaria y fitosanitaria.
+          .col-auto.mb-4.mb-lg-0
+            figure
+              img(src='@/assets/curso/tema2/img06.svg', style="width: 650px").m-auto
+        .row.align-items-center.justify-content-center
+          .col-lg-4
+            h4 CONPES 3376 de 2005. 
+            p Política Sanitaria y de Inocuidad de las Cadenas de la Carne Bovina y la Leche. Encaminada a mejorar las condiciones de sanidad e inocuidad de las cadenas de la carne bovina y la leche, con el fin de proteger la salud y la vida de las personas, los animales, y preservar la calidad del medio ambiente; mejorar la competitividad de estas cadenas y obtener la admisibilidad de sus productos en los mercados internacionales.
+          .col-auto.mb-4.mb-lg-0
+            figure
+              img(src='@/assets/curso/tema2/img07.svg', style="width: 650px").m-auto
+        .row.align-items-center.justify-content-center
+          .col-lg-4
+            h4 CONPES 3676 de 2010. 
+            p Consolidación de la política sanitaria y de inocuidad para las cadenas láctea y cárnica.
+          .col-auto.mb-4.mb-lg-0
+            figure
+              img(src='@/assets/curso/tema2/img08.svg', style="width: 650px").m-auto
+        .row.align-items-center.justify-content-center
+          .col-lg-4
+            h4 Decreto 616 de 2006.
+            p Reglamento Técnico sobre los requisitos que debe cumplir la leche para el consumo humano que se obtenga, procese, envase, transporte, comercialice, expenda, importe o exporte en el país.
+          .col-auto.mb-4.mb-lg-0
+            figure
+              img(src='@/assets/curso/tema2/img09.png', style="width: 650px").m-auto
+        .row.align-items-center.justify-content-center
+          .col-lg-4
+            h4 Resolución 068167 de 2020. 
+            p Política Sanitaria y de Inocuidad de las Cadenas de la Carne Bovina y la Leche. Encaminada a mejorar las condiciones de sanidad e inocuidad de las cadenas de la carne bovina y la leche, con el fin de proteger la salud y la vida de las personas, los animales, y preservar la calidad del medio ambiente; mejorar la competitividad de estas cadenas y obtener la admisibilidad de sus productos en los mercados internacionales.
+          .col-auto.mb-4.mb-lg-0
+            figure
+              img(src='@/assets/curso/tema2/img10.png', style="width: 650px").m-auto
 
-
-
-
-
-
-
+    Separador
+    #t_2_2
+    .titulo-segundo.color-acento-contenido(data-aos="fade-right")
+      h2 2.2 Implicaciones e Importancia
+    .row
+      .col-lg-12.mb-5
+        span.etiqueta #[b De acuerdo con los organismos de referencia a nivel mundial se considera que la leche, la carne y sus derivados ] 
+        span pertenecen al grupo de alimentos de mayor riesgo en salud pública, ya que sus características de composición favorecen la proliferación microbiana y por consiguiente, cualquier deficiencia en sus condiciones de producción, procesamiento, manipulación, conservación, transporte y comercialización puede ocasionar trastornos a la salud del consumidor. La susceptibilidad de estos alimentos para ocasionar enfermedades transmitidas por alimentos que no solo afecta significativamente la salud y bienestar de la población, sino que tiene consecuencias económicas para las personas, familias, comunidades, negocios y en general para los países, ya que imponen cargas en los sistemas de salud.
+    .row.mb-5
+      .col-lg-12
+        .cajon.color-acento-contenido.p-4(data-aos="fade-right")
+          .row.align-items-center
+            .col-auto
+              img(src='@/assets/curso/tema2/img12.svg', style="width: 110px") 
+            .col-lg-6
+              p.mb-0 Uno de los eslabones más importantes es el primario; es decir, las unidades productivas de ganado de diversos tipos. Es por eso, que las políticas del gobierno establecen a través de los diferentes CONPES, decretos y otras normas sobre programas y planes dirigidos a este eslabón, los cuales al pasar el tiempo se reúnen en el programa de Buenas Prácticas Ganaderas BPG que veremos a continuación:
+            .col-lg-4 
+              img(src='@/assets/curso/tema2/img11.png', style="width: 350px") 
 </template>
 <script>
 export default {
